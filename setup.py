ab5910a38eebb136b6d717541e43bb11068b3b37
NilayKumarSahu <121959316+NilayKumarSahu@users.noreply.github.com>
2023-07-24 19:17:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import find_packages,setup
-from typing import List
+from setuptools import find_packages,setup #for creating packages and finding packages in project
+from typing import List #for getting packages in list
 
 HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path:str)->List[str]: #accepting file path in str and returning as list
     requirements=[]
-    with open(file_path)as file_obj:
-        requirements = file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+    with open(file_path)as file_obj: #opening file to read
+        requirements = file_obj.readlines() #reading line by line
+        requirements=[req.replace("\n","")for req in requirements] # removing \n
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT) # removing -e .
 
 
     return requirements
@@ -22,7 +22,7 @@ setup(
     version='0.0.1',
     author='Nilay',
     author_email='nilaykr27@gmail.com',
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=get_requirements('requirements.txt'), # giving requirements.txt file
     packages=find_packages()
 
 
