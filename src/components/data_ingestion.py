513f451d6cb5f7b778e931abd593b085bb8c32f6
NilayKumarSahu <121959316+NilayKumarSahu@users.noreply.github.com>
2023-07-28 14:44:41 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,37 +1,37 @@
-import os
-import sys
+import os #for creating file path
+import sys # for system error
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from dataclasses import dataclass #helps create variables without init
 from src.components.data_transformation import DataTransformation
 
-# Initialize data ingestion configuration
+# Initializing data ingestion configuration
 
 @dataclass
 class DataIngestionconfig:
-    train_data_path:str=os.path.join('artifacts','train.csv')
+    train_data_path:str=os.path.join('artifacts','train.csv') #path for training data
     test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts','raw.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv') #main data
 
-## create a class for Data Ingestion
+# Creating a class for Data Ingestion
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionconfig()
 
     def initiate_data_ingestion(self):
-        logging.info('Data Ingestion methods Starts')
+        logging.info('Data Ingestion method Starts')
         try:
             df=pd.read_csv('notebooks/data/gemstone.csv')
             logging.info('Dataset read as pandas Dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)# exist_ok checks if directory already available
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)# index = false makes sure we don't get additional index
             logging.info('Train test split')
             train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) #header=True for columns name to be written in csv file
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info('Ingestion of Data is completed')
